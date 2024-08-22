971401de3a2c7ea3a743812831b530310fe7daf6
A1bu5 <albuslemonsnowball@proton.me>
2024-08-22 22:20:12 +0800

    Update file_transfer.rs

--- client/src/file_transfer.rs
+++ client/src/file_transfer.rs
@@ -20,10 +20,10 @@ pub fn read_file(file_path: &str) -> io::Result<(Vec<u8>, String)> {
 
 pub fn send_message_or_file(stream: &mut TcpStream, key: &[u8], input: &str) -> io::Result<()> {
     if input.starts_with("/") {
-        // 处理文件传输
+        // File
         send_file(stream, key, input)
     } else {
-        // 处理普通消息
+        // Message
         let encrypted_message = encrypt(key, input.as_bytes()).expect("Message encryption failed");
         stream.write_all(b"MSG\n")?;
         stream.write_all(&encrypted_message)?;
@@ -39,16 +39,16 @@ pub fn send_file(stream: &mut TcpStream, key: &[u8], file_path: &str) -> io::Res
     let file_name = file_path.split('/').last().unwrap_or("unknown").to_string();
     let file_size = buffer.len();
 
-    // 发送文件元数据（文件名和文件大小）
+    // metadataSend
     let metadata = format!("FILENAME:{}\nSIZE:{}\n", file_name, file_size);
     println!("Sending metadata: {}", metadata);
     stream.write_all(metadata.as_bytes())?;
     println!("Metadata sent.");
 
-    // 创建加密器
+    // Encrypt
     let cipher = Aes256Gcm::new(GenericArray::from_slice(key));
 
-    // 分块发送文件数据
+    // Divide chunks
     let chunk_size = 1024;
 
     for (i, chunk) in buffer.chunks(chunk_size).enumerate() {
@@ -60,13 +60,13 @@ pub fn send_file(stream: &mut TcpStream, key: &[u8], file_path: &str) -> io::Res
         let encrypted_chunk = cipher.encrypt(nonce, chunk)
             .expect("File chunk encryption failed");
 
-        // 发送 Nonce 和加密的数据块
+        // Sent Nonce and EncryptedChunks
         stream.write_all(nonce)?;
         stream.write_all(&encrypted_chunk)?;
         println!("Chunk {} sent. Nonce: {:?}", i, nonce);
     }
 
-    // 发送文件传输结束标志
+    // Transfer End sign
     println!("Sending end signal.");
     stream.write_all(b"END\n")?;
     println!("File transmission completed.");
